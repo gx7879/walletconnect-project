8093999d5c6355ddbb9d303151fd322a692ae081
576 <wilsonwu@double-cash.com>
2022-10-24 17:16:29 +0800

    update nft contract address

--- web3/config.js
+++ web3/config.js
@@ -1,7 +1,7 @@
 import WalletConnectProvider from '@walletconnect/web3-provider'
 
 const USDT_CONTRACT_ADDRESS = '0x5894E7526B7a7dc14F16906855F14788E244D42D'
-const NFT_CONTRACT_ADDRESS = '0x5894E7526B7a7dc14F16906855F14788E244D42D'
+const NFT_CONTRACT_ADDRESS = '0xed9A3c81F5b57FdB4467fDB92ff936EF49bE587D'
 
 const providerOptions = {
   walletconnect: {
